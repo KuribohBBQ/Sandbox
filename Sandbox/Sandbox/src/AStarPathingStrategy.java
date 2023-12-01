19ec61ee1a3c49f81669816889e8e964c7fbeb7b
KuribohBBQ <342793euclid@gmail.com>
2023-11-30 19:54:30 -0800

    sandbpx

--- Sandbox/Sandbox/src/AStarPathingStrategy.java
+++ Sandbox/Sandbox/src/AStarPathingStrategy.java
@@ -22,20 +22,52 @@ class AStarPathingStrategy
         List<Point> path = new LinkedList<Point>();
 
         Node current = new Node(start, null);
-        List<Point> neighbors = potentialNeighbors.apply(current.getPoint()).filter(canPassThrough).toList();
+//        List<Point> neighbors = potentialNeighbors.apply(current.getPoint()).filter(canPassThrough).toList();
 
 
         ArrayList<Node> openlist = new ArrayList<>();
         ArrayList<Node> closedlist = new ArrayList<>();
         openlist.add(current);
-
+        double g = 0;
+        double h = 0;
+        double f = 0;
 
         while (!openlist.isEmpty())
         {
+            List<Point> neighbors = potentialNeighbors.apply(current.getPoint()).filter(canPassThrough).toList();
+            //analyze valid adjacent nodes that are not on closed list
+            for (Point n: neighbors)
+            {
+                Node PotentialNode = new Node(n, current);
+                //checks if node is already on openlist
+                for (Node check: openlist)
+                {
+                    if (check.getPoint() == PotentialNode.getPoint())
+                    {
+                        //checking if calculated g is better than previously calculated g
+                        double calc_g = Manhattan(PotentialNode, current);
+                        if (calc_g > g)
+                        {
+                            g = calc_g;
+                        }
+                        //estimate distance of potential node to end point
+                        h = Manhattan(PotentialNode, end);
+                    }
+                    //determine distance from start node(g value)
+                    else
+                    {
+                       double new_g = Manhattan
+                    }
+                }
+
+
+            }
+
         if (withinReach.test(current.getPoint(), end)) {
             // build path and return
         }
-        }
+
+        } //end of while loop
 
         return path;
     }
